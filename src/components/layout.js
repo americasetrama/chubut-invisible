d3b27cd56cb3f0a82271d9be5d0ae6a983b4169e
santuan <smrcouto88@gmail.com>
2020-04-07 15:55:47 -0300

    pages

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,7 +8,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
-import Sidebar from "../components/Sidebar"
+import Sidebar from "../components/sidebar"
 import Navigation from "../components/navigation"
 
 import Header from "./header"
